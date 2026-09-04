8d9893f1e46563f4b9a98dc405db625ea5752e93
Celestia74 <77862536+Celestia74@users.noreply.github.com>
2026-09-03 22:20:02 -0500

    *sigh* forgot to lint

--- calc/src/mechanics/gen789.ts
+++ calc/src/mechanics/gen789.ts
@@ -141,7 +141,7 @@ export function calculateSMSSSV(
     move.flags.contact = 1;
   }
 
-  const breaksProtect = !defender.isDynamaxed && 
+  const breaksProtect = !defender.isDynamaxed &&
   (move.breaksProtect || move.isZ || attacker.isDynamaxed ||
   (attacker.hasAbility('Unseen Fist', 'Piercing Drill') && move.flags.contact)) ||
   move.name === 'G-Max One Blow' || move.name === 'G-Max Rapid Flow';
